b956a26ab96677d76099621edbc462a3ff50e504
Vincentius Filbert Amadeo <vincentius.filbert@ui.ac.id>
2026-02-20 20:06:03 +0700

    feat: add pg support

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("com.h2database:h2")
 
     compileOnly("org.projectlombok:lombok")
